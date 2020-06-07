526905efb2bb737414f23c4b85446e1739782336
Nihal Varia <nihalsinghvaria@gmail.com>
2020-06-07 17:29:56 +0530

    Added DM Mono fonts

--- src/Organisms/Home/Home.js
+++ src/Organisms/Home/Home.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Home.scss";
 
 function Home() {
+
+    const [displayState, setDisplayState] = useState("")
+    const headerText = useRef(null)
+
+    useEffect(() => {
+    })
+
     return (
         <div className="Home position-relative flex h-100 w-100">
             <div className="center-con flex flex-column text-center">
-                <h1 className="title">nihal.dev</h1>
+                <h1 ref={headerText} className="title line-1 anim-typewriter">nihal.dev</h1>
                 <h2 className="subtitile">subtitle</h2>
                 <p>
                     my work
