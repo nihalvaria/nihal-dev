9cbf3641704daa1b99671b1bfdc7815cb6cf3bd7
Nihal Varia <nihalsinghvaria@gmail.com>
2020-06-07 14:22:56 +0530

    Added Home Organism

---
+++ src/Organisms/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.scss";
+
+function Home() {
+    return (
+        <div className="Home h-100 w-100">
+        </div>
+    );
+}
+
+export default Home;
--- src/Templates/MainPage/MainPage.js
+++ src/Templates/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./MainPage.scss";
 import ThreeBG from "../../Molecules/ThreeBG/ThreeBG";
+import Home from "../../Organisms/Home/Home"
 
 function MainPage() {
     return (
-        <div className="MainPage">
+        <div className="MainPage h-100 w-100">
+            <Home />
             <ThreeBG />
         </div>
     );
