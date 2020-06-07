7fa8a169e736fdbee99e9c6f312e9c4f6c0eea8b
Nihal Varia <nihalsinghvaria@gmail.com>
2020-06-07 14:42:44 +0530

    Added basic home template, not sure if this is it

--- src/Organisms/Home/Home.js
+++ src/Organisms/Home/Home.js
@@ -3,7 +3,17 @@ import "./Home.scss";
 
 function Home() {
     return (
-        <div className="Home position-relative h-100 w-100">
+        <div className="Home position-relative flex h-100 w-100">
+            <div className="center-con flex flex-column text-center">
+                <h1 className="title">nihal.dev</h1>
+                <h2 className="subtitile">subtitle</h2>
+                <p>
+                    my work
+                </p>
+                <p>
+                    <a href="mailto:nihalsinghvaria@gmail.com">contact me</a>
+                </p>
+            </div>
         </div>
     );
 }
