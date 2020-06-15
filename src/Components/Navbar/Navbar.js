8abbb7f415d60115b41eb409111cc987cf7ac841
Nihal Varia <nihalsinghvaria@gmail.com>
2020-06-15 16:35:23 +0530

    Added Link to navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,29 @@
 import React, { useContext } from "react";
 import { Context } from "../../Context/Context"
+import { Link } from "react-router-dom"
+import Typewriter from "typewriter-effect";
 import "./Navbar.scss"
 
 const Navbar = props => {
     const { navState } = useContext(Context)
 
     return (
-        <nav className="Navbar position-absolute w-100 flex bg-dark text-white">
-            <div className="curr-page">
+        <nav className="Navbar position-absolute w-100 flex justify-content-between bg-dark text-white">
+            <div className="curr-page mx-4">
                 <p className="curr-page-text m-0">{navState}</p>
             </div>
-            <div className="title">
-                <p className="title-text m-0"></p>
+            <div className="title mx-4">
+                <Link className="title-text m-0 text-white" href="/">
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString(`nihal.dev`)
+                                .callFunction(() => {
+                                })
+                                .start();
+                        }}
+                    />
+                </Link>
             </div>
         </nav>
     );
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,28 +1,14 @@
-import React, { useState, useRef, useEffect } from "react";
-import Typewriter from "typewriter-effect";
+import React, { useEffect } from "react";
 import "./Home.scss";
 
 const Home = props => {
-    const [displayState, setDisplayState] = useState("");
-    const headerText = useRef(null);
 
     useEffect(() => {});
 
     return (
         <div className="Home position-relative flex h-100 w-100">
             <div className="center-con flex flex-column text-center">
-                <h1 ref={headerText} className="title line-1 anim-typewriter">
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            typewriter
-                                .typeString(`nihal.dev`)
-                                .callFunction(() => {
-                                    console.dir(headerText.current.children)
-                                })
-                                .start();
-                        }}
-                    />
-                </h1>
+                
                 <h2 className="subtitile">subtitle</h2>
                 <p>my work</p>
                 <p>
